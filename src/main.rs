be8bce7e7135946a16ad50e100764d9a0082ccde
Guilherme Prado <grprado.bsb@gmail.com>
2020-04-18 05:46:43 -0300

    Forgot to rename a variable

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use chip8::Chip8;
 use multimedia::Multimedia;
 
 fn main() {
-    let screen  = Multimedia::new();
-    let mut chip8 = Chip8::new(screen);
+    let multimedia = Multimedia::new();
+    let mut chip8 = Chip8::new(multimedia);
 
 
     let args = &std::env::args().collect::<Vec<String>>();
@@ -14,7 +14,6 @@ fn main() {
         let rom_file = &args[1];
         chip8.load_rom(rom_file);
     } else {
-        // chip8.load_rom("roms/test_opcode.ch8");
         chip8.load_rom("roms/Pong (alt).ch8");
     }
     while chip8.is_on(){
