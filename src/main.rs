38abb24410230171454f3213ff2b3ff3ba02bba7
Guilherme Prado <grprado.bsb@gmail.com>
2020-04-18 06:25:30 -0300

    Error msg when no rom param

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,34 @@ use chip8::Chip8;
 use multimedia::Multimedia;
 
 fn main() {
+    let args = &std::env::args().collect::<Vec<String>>();
+
+    let rom_file:Option<&str> =
+        if args.len() > 1 {
+            let rom_file = &args[1];
+            Some(rom_file)
+        } else if cfg!(debug_assertions) {
+            Some("roms/PONG2.ch8")
+        } else {
+            None
+        };
+
+    match rom_file {
+        Some(file) => run_chip8(file),
+        None => {
+            eprintln!("Chipr-8 needs a ROM to run, please run: \nchipr-8 rom_file.ch8");
+            std::process::exit(1);
+        }
+    }
+
+}
+
+fn run_chip8(rom_file: &str) {
     let multimedia = Multimedia::new();
     let mut chip8 = Chip8::new(multimedia);
+    chip8.load_rom(rom_file);
 
-
-    let args = &std::env::args().collect::<Vec<String>>();
-    if args.len() > 1 {
-        let rom_file = &args[1];
-        chip8.load_rom(rom_file);
-    } else {
-        chip8.load_rom("roms/PONG2.ch8");
-    }
-    while chip8.is_on(){
+    while chip8.is_on() {
         chip8.execute_cycle();
     }
-
 }
